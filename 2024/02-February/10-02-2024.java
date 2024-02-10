6015dc02b1f60877197c5a1aaabbfc23d9181837
chinmoy1809 <chinmoycode74@gmail.com>
2024-02-10 22:14:31 +0530

    10-02-2024

---
+++ 2024/02-February/10-02-2024.java
@@ -0,0 +1,33 @@
+
+// Name : Chinmoy Das
+// Date : 10/02/2024
+// Problem : Number-of-paths-in-a-matrix-with-k-coins
+// Difficulty level : Medium
+// Problem Link : https://www.geeksforgeeks.org/problems/number-of-paths-in-a-matrix-with-k-coins2728/1
+
+class Solution {
+    long numberOfPath(int n, int k, int[][] arr) {
+        // code here
+        long dp[][][] = new long[n][n][k + 1];
+        if (arr[n - 1][n - 1] <= k) {
+            dp[n - 1][n - 1][arr[n - 1][n - 1]] = 1;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                for (int m = 1; m <= k; m++) {
+
+                    if (m - arr[i][j] >= 0 && i + 1 < n) {
+                        dp[i][j][m] += dp[i + 1][j][m - arr[i][j]];
+                    }
+                    if (m - arr[i][j] >= 0 && j + 1 < n) {
+                        dp[i][j][m] += dp[i][j + 1][m - arr[i][j]];
+                        ;
+                    }
+                }
+            }
+        }
+        return dp[0][0][k];
+    }
+}
+
+// Thankyou❤️‍🩹
