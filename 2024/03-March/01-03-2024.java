ffd428e15f6b7c242b3ac0dded445430378c3b4b
chinmoy1809 <chinmoycode74@gmail.com>
2024-03-02 09:04:45 +0530

    01-02-2024

--- 2024/03-March/01-03-2024.java
+++ 2024/03-March/01-03-2024.java
@@ -1,4 +1,28 @@
-// Keep coding 
+
+// Name : Chinmoy Das
+// Date : 01/03/2024
+// Problem : Peak-element
+// Difficulty level : medium
+// Problem Link : https://www.geeksforgeeks.org/problems/peak-element/1
+
+class Solution {
+    // Function to find the peak element
+    // arr[]: input array
+    // n: size of array a[]
+    public int peakElement(int[] arr, int n) {
+        // add code here.
+        int s = 0;
+        int e = n - 1;
+        while (s != e) {
+            int mid = (s + e) / 2;
+            if (arr[mid] < arr[mid + 1])
+                s = mid + 1;
+            else
+                e = mid;
+        }
+        return s;
+    }
+}
 
 
-// Thankyou❤️‍🩹❤️‍🩹
+// Thankyou❤️‍🩹
