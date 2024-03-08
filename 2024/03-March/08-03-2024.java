724c1328a9ddc71fa030821c17e317d7ab22744f
chinmoy1809 <chinmoycode74@gmail.com>
2024-03-08 21:25:04 +0530

    08-03-2024

---
+++ 2024/03-March/08-03-2024.java
@@ -0,0 +1,47 @@
+
+// Name : Chinmoy Das
+// Date : 08/03/2024
+// Problem : Check frequencies
+// Difficulty level : Easy
+// Problem Link : https://www.geeksforgeeks.org/problems/check-frequencies4211/1
+
+class Solution {
+    boolean sameFreq(String s) {
+        // code here
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray())
+            freq[ch - 'a']++;
+
+        if (check(freq))
+            return true;
+
+        // reduce all ch freq one by one
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                freq[i]--;
+
+                if (check(freq))
+                    return true;
+
+                freq[i]++;
+            }
+        }
+        return false;
+    }
+
+    boolean check(int freq[]) {
+        int n = -1;// get firstNonZero
+        for (int f : freq) {
+            if (f > 0) {
+                if (n == -1)
+                    n = f;
+                else if (f != n)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+}
+
+// Thankyou❤️‍🩹
